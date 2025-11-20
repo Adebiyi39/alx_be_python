c7725ca46bb2786066ba8dbf593f24ea72239a40
Adebiyi39 <oluwatobilobaa349@gmail.com>
2025-11-20 17:55:33 +0100

    control-flow

--- python_introduction/future_age_calculator.py
+++
@@ -1,8 +0,0 @@
-# prompt the user for their current age
-current_age = int(input("How are you? "))
-
-#calculate age in 2050 ( 2050 - 2030 = 27 years later)
-age_in_2050 = current_age  + 27
-
-#print the results
-print(f"In 2050, you will be {age_in_2050} years old.")
