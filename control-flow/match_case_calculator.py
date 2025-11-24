17e1765c19102886dc5ec81c589a7cf4233fd119
Adebiyi39 <151687116+Adebiyi39@users.noreply.github.com>
2025-11-24 13:13:05 -0800

    Add files via upload

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,6 @@
+#prompt user forr two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+#Ask the type of operation
+operation = input("choose the operation (+, -, *, /): ")
