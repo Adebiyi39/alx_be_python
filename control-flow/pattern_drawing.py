853b57b3d8af7b255649d523cd0733acde04b219
Adebiyi39 <151687116+Adebiyi39@users.noreply.github.com>
2025-11-27 05:28:21 -0800

    Add files via upload

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,14 @@
+#Prompt user for pattern size
+size = int(input("Enter the size of the paatern: "))
+
+# Intialized row counter
+row = 0
+
+# Draw the pattern
+while row < size:
+    # print each row using a for loop
+    for _ in range (size):
+        print("*", end="")
+        print() # Move to the next line
+        row += 1
+
