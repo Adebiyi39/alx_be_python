1cce447c2a2f4f97b347a4c4240fecc50c4d731a
Adebiyi39 <oluwatobilobaa349@gmail.com>
2025-12-15 15:30:58 +0100

    oop

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,4 @@
+#  Part 1: Display Current Date and Time
+def display_current_datetime():
+    current_date = datetime.now()
+    
---
+++ fns_and_dsa/scope.py
@@ -0,0 +1,10 @@
+x = 'global x'
+
+def test():
+    global x
+    x = 'local x'
+    # print(y)
+    print(x)
+
+test()
+print(x)
